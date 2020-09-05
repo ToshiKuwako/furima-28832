58bedba2264333407d922e1d9edd3ca574b3dae2
ToshiKuwako <toshiki.se@gmail.com>
2020-09-05 23:36:22 +0900

    バリデーション ,ビュー

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-end
+        
+  with_options presence: true do
+    validates :nickname, 
+    validates :email,             uniqueness: true,inclusion: {in:@}
+    validates :password,          length: { minimum: 6 },format: { with:/^[a-z0-9]+$/}
+    validates :first_name,        format: { with:}
+    validates :family_name
+    validates :first_name_kana
+    validates :family_name_kana
+    validates :birth_date
+    
+    
+  end
+
+
+        end
