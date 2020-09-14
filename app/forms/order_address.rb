06c683ca167e2ab8cde003570f4088779f509450
ToshiKuwako <toshiki.se@gmail.com>
2020-09-14 18:41:33 +0900

    card.js生成、編集

--- app/forms/order_address.rb
+++ app/forms/order_address.rb
@@ -15,4 +15,4 @@ include ActiveModel::Model
     Address.create(postal_code: postal_code, prefecture: prefecture, city: city, house_number: house_number, building_name: building_name)
     Order.create(user_id: user_id, item_id: item_id)
   end
-  end
+end
