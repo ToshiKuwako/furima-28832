4d9c514de1954206aa46bb53fb2ee218e7fbc67c
ToshiKuwako <toshiki.se@gmail.com>
2020-09-16 17:40:19 +0900

    購入ページへの遷移を制限、単体エラーコード実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,16 @@
 class OrdersController < ApplicationController
+ before_action :move_to_root_path , only:[:index]
+
   def index
+   
     @item = Item.find(params[:item_id])
     @order = OrderAddress.new(order_params)
-   
+  
+  
   end
+
+   
+
 
   def create
     @order = OrderAddress.new(order_params)
@@ -18,6 +25,16 @@ class OrdersController < ApplicationController
   end
 
   private
+
+  def move_to_root_path
+    @item = Item.find(params[:item_id])
+
+    if  current_user.id == @item.user_id || @item.order != nil 
+      redirect_to root_path
+    elsif @item.order == @item.id 
+      redirect_to root_path
+    end
+  end
 
   def order_params
     params.permit( :zip_code, :prefecture_id,:city, :house_number, :building, :phone_number,:token, :authenticity_token)
