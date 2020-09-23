cc7b3c1942a2cca54b41d2e86979bad52a7c024c
ToshiKuwako <toshiki.se@gmail.com>
2020-09-23 20:30:39 +0900

    マイページのビュー作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,25 @@ class UsersController < ApplicationController
 
   def create
   end
+
+  def show
+   
+  end
+
+  def update
+    if current_user.update(user_params) 
+      redirect_to root_path 
+    else
+      redirect_to "show" 
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :email)
+  end
+  
+  
+    
 end
